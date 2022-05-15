500b470db3278149a35c94d02b39ef73d786572c
philipRatsovPP <61462001+philipRatsovPP@users.noreply.github.com>
2022-05-15 22:25:21 +0300

    feat: added response models for get and list

---
+++ Api/ApiDomain/Surveys/ListSurveysResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using task_app.Domain.Surveys;
+
+#pragma warning disable CS8618
+
+namespace task_app.ApiDomain.Surveys
+{
+    public class ListSurveysResponse
+    {
+        [Required]
+        public Survey[] Surveys { get; set; }
+    }
+}
