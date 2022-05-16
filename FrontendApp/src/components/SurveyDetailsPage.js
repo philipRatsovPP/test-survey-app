cc47fc8129ac08b15cd68d74eafe0327622b35fa
philipRatsovPP <61462001+philipRatsovPP@users.noreply.github.com>
2022-05-16 23:30:57 +0300

    feat: added survey forms

--- FrontendApp/src/components/SurveyDetailsPage.js
+++ FrontendApp/src/components/SurveyDetailsPage.js
@@ -1,26 +1,112 @@
 import React, { useState, useEffect } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap'
 import { useParams } from 'react-router-dom';
 
 export function SurveyDetailsPage() {
 
     const params = useParams();
+    const id = params.id;
     const [survey, setSurvey] = useState(null);
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [age, setAge] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
-            const response = await fetch(`survey/surveys/12`);
+            const response = await fetch(`survey/surveys/${id}`);
             const data = await response.json();
             setSurvey(data.survey);
         }
         fetchData()
     }, [])
 
-    console.log(params);
-    console.log(survey);
+    const handleSubmit = async (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        await fetch(`survey/response/${id}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                email,
+                age,
+            })
+        });
+    };
+
+    if (!survey) { return null; }
+
+    const firstNameFormProperty = survey.properties.firstName;
+    const lastNameFormProperty = survey.properties.lastName;
+    const emailFormProperty = survey.properties.email;
+    const ageFormProperty = survey.properties.age;
 
     return (
         <div>
-            A
+            <Form>
+                <FormGroup style={{ marginBottom: '16px' }}>
+                    <Label for="firstName">
+                        {firstNameFormProperty.label}
+                    </Label>
+                    <Input
+                        id="firstName"
+                        name="firstName"
+                        placeholder={firstNameFormProperty.label}
+                        type="text"
+                        required={firstNameFormProperty.required}
+                        invalid={firstName === ''}
+                        onChange={(e) => { setFirstName(e.target.value) }}
+                    />
+                </FormGroup>
+                <FormGroup style={{ marginBottom: '16px' }}>
+                    <Label for="lastName">
+                        {lastNameFormProperty.label}
+                    </Label>
+                    <Input
+                        id="lastName"
+                        name="lastName"
+                        placeholder={lastNameFormProperty.label}
+                        type="text"
+                        invalid={lastName === ''}
+                        required={lastNameFormProperty.required}
+                        onChange={(e) => { setLastName(e.target.value) }}
+                    />
+                </FormGroup>
+                <FormGroup style={{ marginBottom: '16px' }}>
+                    <Label for="email">
+                        {emailFormProperty.label}
+                    </Label>
+                    <Input
+                        id="email"
+                        name="email"
+                        placeholder={emailFormProperty.label}
+                        type="text"
+                        invalid={email === ''}
+                        required={emailFormProperty.required}
+                        onChange={(e) => { setEmail(e.target.value) }}
+                    />
+                </FormGroup>
+                <FormGroup style={{ marginBottom: '16px' }}>
+                    <Label for="age">
+                        {ageFormProperty.label}
+                    </Label>
+                    <Input
+                        id="age"
+                        name="age"
+                        placeholder={ageFormProperty.label}
+                        type={ageFormProperty.type}
+                        required={ageFormProperty.required}
+                        onChange={(e) => { setAge(e.target.value) }}
+                    />
+                </FormGroup>
+                <Button onClick={handleSubmit} type="submit">Submit survey</Button>
+            </Form>
         </div>
     )
 }
