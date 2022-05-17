8a2be1d444bfd34f622cb5fd470c8f3f3da5ad60
philipRatsovPP <61462001+philipRatsovPP@users.noreply.github.com>
2022-05-17 07:40:02 +0300

    chore: removed commented out code

--- FrontendApp/src/App.js
+++ FrontendApp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Surveys } from './components/Surveys';
@@ -11,7 +11,6 @@ export function App() {
     <Layout>
       <Route exact path='/' component={Surveys} />
       <Route exact path='/surveys' component={Surveys} />
-      {/* <Route path='/surveys/:id' component={SurveyDetailsPage} /> */}
       <Route path='/surveys/:id'>
         <SurveyDetailsPage />
       </Route>
